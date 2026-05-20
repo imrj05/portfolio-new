1f7ecfee45c71b43af5380ef126ab163102d3250
work-rjkashyap <work.rjkashyap05@gmail.com>
2026-05-21 01:01:08 +0530

    refactor: remove AnalyticsProvider from main rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AnalyticsProvider } from './lib/analytics.tsx'
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AnalyticsProvider>
-            <App />
-        </AnalyticsProvider>
+        <App />
     </StrictMode>,
 )
